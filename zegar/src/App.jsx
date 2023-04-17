c5250e86fc5486eb9b6a298aff5e9e5a966492f4
Kamci0 <mgkaml@wp.pl>
2023-04-17 14:11:08 +0200

    zegar w jsx

---
+++ zegar/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Zegar from "./Zegar";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Zegar/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ zegar/src/Zegar.jsx
@@ -0,0 +1,527 @@
+import React from "react";
+
+function Zegar(){
+  return (
+    <div>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="210mm"
+      height="297mm"
+      version="1.1"
+      viewBox="0 0 210 297"
+    >
+       <defs>
+        <linearGradient id="linearGradient979">
+          <stop offset="0.152" stopColor="#bcbcbc" stopOpacity="1"></stop>
+          <stop offset="0.869" stopColor="#7d7d7d" stopOpacity="1"></stop>
+        </linearGradient>
+        <linearGradient
+          id="linearGradient981"
+          x1="38.571"
+          x2="185.494"
+          y1="98.708"
+          y2="98.708"
+          gradientUnits="userSpaceOnUse"
+          xlinkHref="#linearGradient979"
+        ></linearGradient>
+      </defs>
+      <g>
+        <circle
+          cx="112.032"
+          cy="98.708"
+          r="73.461"
+          fill="#999"
+          strokeWidth="0.265"
+        ></circle>
+        <circle
+          cx="112.032"
+          cy="98.708"
+          r="73.461"
+          fill="url(#linearGradient981)"
+          fillOpacity="1"
+          strokeWidth="0.265"
+        ></circle>
+        <circle
+          cx="112.558"
+          cy="99.409"
+          r="69.253"
+          fill="#e6e6e6"
+          fillOpacity="1"
+          strokeWidth="0.249"
+        ></circle>
+        <circle
+          id="center"
+          cx="112.032"
+          cy="98.708"
+          r="2.06"
+          fill="#1a1a1a"
+          fillOpacity="1"
+          strokeWidth="0.007"
+        ></circle>
+        <path
+          id="seconds-hand"
+          fill="#1a1a1a"
+          fillOpacity="1"
+          strokeWidth="0.265"
+          transform="rotate(0, 112, 98.5)"
+          d="M111.51 46.959H112.497V99.023H111.51z"
+        ></path>
+        <path
+          id="hours-hand"
+          fill="#1a1a1a"
+          fillOpacity="1"
+          strokeWidth="0.265"
+          transform="rotate(0, 112, 98.5)"
+          d="M110.572 85.696H113.711V99.078H110.572z"
+        ></path>
+        <path
+          id="minutes-hand"
+          fill="#1a1a1a"
+          fillOpacity="1"
+          strokeWidth="0.265"
+          transform="rotate(0, 112, 98.5)"
+          d="M111.092 72.59H113.401V98.69500000000001H111.092z"
+        ></path>
+        <path
+          id="rect521"
+          fill="#1a1a1a"
+          strokeWidth="0.181"
+          d="M112.032 31.12H113.084V37.087H112.032z"
+        ></path>
+        <use
+          id="use1920"
+          x="0"
+          y="0"
+          transform="matrix(1.83352 0 0 1.30907 -93.568 -9.618)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(6 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(12 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(18 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(24 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(30 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(36 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(42 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(48 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(54 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(60 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(66 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(72 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(78 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(84 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(90 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(96 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(102 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(108 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(114 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(120 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(126 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(132 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(138 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(144 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(150 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(156 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(162 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(168 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(174 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(180 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-174 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-168 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-162 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-156 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-150 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-144 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-138 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-132 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-126 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-120 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-114 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-108 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-102 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-96 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-90 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-84 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-78 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-72 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-66 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-60 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-54 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-48 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-42 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-36 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-30 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-24 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-18 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-12 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-6 112.032 98.842)"
+          xlinkHref="#rect521"
+        ></use>
+        <use x="0" y="0" xlinkHref="#use1920"></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(30 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(60 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(90 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(120 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(150 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(180 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-150 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-120 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-90 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-60 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+        <use
+          x="0"
+          y="0"
+          transform="rotate(-30 112.045 98.706)"
+          xlinkHref="#use1920"
+        ></use>
+      </g>
+    </svg>
+    </div>
+  );
+}
+
+export default Zegar;
