cb8341c74738f6113c5adf1b0eb2b9d3a98d88ec
Kamci0 <mgkaml@wp.pl>
2023-04-24 15:58:28 +0200

    do zrobienia

--- zegar/src/App.jsx
+++ zegar/src/App.jsx
@@ -11,11 +11,11 @@ class App extends React.Component{
                 minuta: -1,
                 sekunda: -1
             },
-            Czas_shift:{
-                godzina: 0,
-                minuta: 0,
-                sekunda: 0
-            },
+            // Czas_shift:{
+            //     godzina: 0,
+            //     minuta: 0,
+            //     sekunda: 0
+            // },
             zmiana: false
         }
         this.componentDidMount = this.componentDidMount.bind(this);
@@ -25,19 +25,24 @@ class App extends React.Component{
         this.handle_click = this.handle_click.bind(this);
         this.componentWillUnmount = this.componentWillUnmount.bind(this);
         this.handle_reset = this.handle_reset.bind(this);
-        this.odswierzanie = this.odswierzanie.bind(this);
+        this.odswiezanie = this.odswiezanie.bind(this);
     }
 
 
     handle_reset(){
         this.setState({zmiana: false})
         this.setState(()=>({
-            Czas_shift:{
-                godzina: 0,
-                minuta: 0,
-                sekunda: 0
+            // Czas_shift:{
+            //     godzina: 0,
+            //     minuta: 0,
+            //     sekunda: 0
+            // }
+            Czas:{
+                godzina: new Date().getHours(),
+                minuta: new Date().getMinutes(),
+                sekunda: new Date().getSeconds()
             }
-        }),()=>(localStorage.setItem('czas_shift', JSON.stringify(this.state.Czas_shift))));
+        }),()=>((localStorage.setItem('Czas', JSON.stringify(this.state.Czas))), (localStorage.setItem('zmiana', JSON.stringify(this.state.zmiana)))));
     }
 
     handle_click(){
@@ -59,20 +64,25 @@ class App extends React.Component{
 
 
         this.setState(()=>({
-            Czas_shift:{
-                godzina: Number(this.state.Czas_shift.godzina) + Number(h),
-                minuta: Number(this.state.Czas_shift.minuta) + Number(m),
-                sekunda: Number(this.state.Czas_shift.sekunda) + Number(s)
+            // Czas_shift:{
+            //     godzina: Number(this.state.Czas_shift.godzina) + Number(h),
+            //     minuta: Number(this.state.Czas_shift.minuta) + Number(m),
+            //     sekunda: Number(this.state.Czas_shift.sekunda) + Number(s)
+            // }
+            Czas:{
+                godzina: Number(h),
+                minuta: Number(m),
+                sekunda: Number(s)
             }
-        }), ()=>(localStorage.setItem('czas_shift', JSON.stringify(this.state.Czas_shift))));
+        }), ()=>((localStorage.setItem('Czas', JSON.stringify(this.state.Czas))), (localStorage.setItem('zmiana', JSON.stringify(this.state.zmiana)))));
 
-        this.setState({
-            Czas: {
-              godzina: this.state.Czas.godzina + Number(this.state.Czas_shift.godzina),
-              minuta: this.state.Czas.minuta + Number(this.state.Czas_shift.minuta),
-              sekunda: this.state.Czas.sekunda + Number(this.state.Czas_shift.sekunda),
-            }
-        });
+        // this.setState({
+        //     Czas: {
+        //       godzina: this.state.Czas.godzina + Number(this.state.Czas_shift.godzina),
+        //       minuta: this.state.Czas.minuta + Number(this.state.Czas_shift.minuta),
+        //       sekunda: this.state.Czas.sekunda + Number(this.state.Czas_shift.sekunda),
+        //     }
+        // });
 
         this.setState({zmiana: true})
 
@@ -86,7 +96,12 @@ class App extends React.Component{
         console.log("componentWillMount");
     }
 
-    odswierzanie(){
+    odswiezanie(){
+        const shift = JSON.parse(localStorage.getItem("Czas")) || {godzina: 0, minuta: 0, sekunda: 0}; 
+        const change = JSON.parse(localStorage.getItem("zmiana")) || false;
+            
+        this.setState({zmiana: change});
+
         if(!this.state.zmiana){
             this.setState({
                 Czas:{
@@ -96,11 +111,26 @@ class App extends React.Component{
                 }
             });
         }else{
+            var zmiana_czasu = {
+                godzina:  shift.godzina - new Date().getHours(),
+                minuta: shift.minuta - new Date().getMinutes(),
+                sekunda: shift.sekunda - new Date().getSeconds()
+            }
             this.setState({
+                // Czas:{
+                //     godzina: new Date().getHours()+ Number(this.state.Czas_shift.godzina),
+                //     minuta: new Date().getMinutes()+ Number(this.state.Czas_shift.minuta),
+                //     sekunda: new Date().getSeconds()+ Number(this.state.Czas_shift.sekunda)
+                // }
+                // Czas:{
+                //     godzina: new Date().getHours() + zmiana_czasu.godzina,
+                //     minuta: new Date().getMinutes() + zmiana_czasu.minuta,
+                //     sekunda: new Date().getSeconds() + zmiana_czasu.sekunda
+                // }
                 Czas:{
-                    godzina: new Date().getHours()+ Number(this.state.Czas_shift.godzina),
-                    minuta: new Date().getMinutes()+ Number(this.state.Czas_shift.minuta),
-                    sekunda: new Date().getSeconds()+ Number(this.state.Czas_shift.sekunda)
+                    godzina: new Date().getHours() - zmiana_czasu['godzina'],
+                    minuta: new Date().getMinutes() - zmiana_czasu['minuta'],
+                    sekunda: new Date().getSeconds() - zmiana_czasu['sekunda']
                 }
             }); 
         }    
@@ -108,52 +138,60 @@ class App extends React.Component{
 
     componentDidMount(){
         console.log("componentDidMount");
-        const shift = JSON.parse(localStorage.getItem("czas_shift")) || {godzina: 0, minuta: 0, sekunda: 0};
+        const change = JSON.parse(localStorage.getItem("zmiana")) || false;
+            
 
-        console.log(shift)
+        this.setState({zmiana: change})
+
+        // if(change){
+        //     this.setState({
+        //         Czas:{
+        //             godzina: shift.godzina,
+        //             minuta: shift.minuta,
+        //             sekunda: shift.sekunda
+        //         }
+        //     })
+        // }
+        // if(shift.minuta >= 60 ){
+        //     shift.godzina+=1;
+        //     shift.minuta-=60;
+        // }
+
+        // if(shift.sekunda >= 60){
+        //     shift.minuta+=1;
+        //     shift.sekunda-=60;
+        // }
+
+        // this.setState(()=>({
+        //     Czas_shift:{
+        //         godzina: shift.godzina,
+        //         minuta: shift.minuta,
+        //         sekunda: shift.sekunda
+        //     }
+        // }))
 
         
 
-        if(shift.minuta >= 60 ){
-            shift.godzina+=1;
-            shift.minuta-=60;
-        }
+        // if(shift.godzina != 0 || shift.minuta != 0 || shift.sekunda != 0){
+        //     this.setState({zmiana: true})
+        //     this.setState({
+        //         Czas: {
+        //           godzina: new Date().getHours() + Number(this.state.Czas_shift.godzina),
+        //           minuta: new Date().getMinutes() + Number(this.state.Czas_shift.minuta),
+        //           sekunda: new Date().getSeconds() + Number(this.state.Czas_shift.sekunda),
+        //         }
+        //     });
+        //     console.log("zmiana")
+        // }else{
+        //     this.setState({
+        //         Czas:{
+        //             godzina: new Date().getHours(),
+        //             minuta: new Date().getMinutes(),
+        //             sekunda: new Date().getSeconds()
+        //     }})
+        // }
 
-        if(shift.sekunda >= 60){
-            shift.minuta+=1;
-            shift.sekunda-=60;
-        }
-
-        this.setState(()=>({
-            Czas_shift:{
-                godzina: shift.godzina,
-                minuta: shift.minuta,
-                sekunda: shift.sekunda
-            }
-        }))
-
-        
-
-        if(shift.godzina != 0 || shift.minuta != 0 || shift.sekunda != 0){
-            this.setState({zmiana: true})
-            this.setState({
-                Czas: {
-                  godzina: new Date().getHours() + Number(this.state.Czas_shift.godzina),
-                  minuta: new Date().getMinutes() + Number(this.state.Czas_shift.minuta),
-                  sekunda: new Date().getSeconds() + Number(this.state.Czas_shift.sekunda),
-                }
-            });
-            console.log("zmiana")
-        }else{
-            this.setState({
-                Czas:{
-                    godzina: new Date().getHours(),
-                    minuta: new Date().getMinutes(),
-                    sekunda: new Date().getSeconds()
-            }})
-        }
-
-        setInterval(this.odswierzanie, 1000);
+        setInterval(this.odswiezanie, 1000);
     }
 
     //aktualizacja
@@ -165,14 +203,13 @@ class App extends React.Component{
 
     componentDidUpdate(){
         console.log("ComponentDidUpdate");
-        localStorage.setItem('czas_shift', JSON.stringify(this.state.Czas_shift));
     }
 
     //odmontowywanie
 
     componentWillUnmount(){
         console.log("componentWillUnmount")
-        clearInterval(this.odswierzanie);
+        clearInterval(this.odswiezanie);
     }
 
     render(){
@@ -199,12 +236,12 @@ class App extends React.Component{
                         <button onClick={this.handle_click}>Dodaj czas</button>
                         <button onClick={this.handle_reset}>Resetuj czas</button>
                     </div>
-                    <div>
-                        Przesunięcie
+                    {/* <div>
+                        Przesunięcie -  
                         {this.state.Czas_shift.godzina}:
                         {this.state.Czas_shift.minuta}:
                         {this.state.Czas_shift.sekunda}
-                    </div>
+                    </div> */}
                 </div>
                 </div>
                 
